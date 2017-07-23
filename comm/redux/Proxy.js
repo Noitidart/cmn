352caa9ffa5126787d46383be6d78ba5e802582a
Yasir <yasir.ali@email.ucr.edu>
2017-07-23 04:48:52 -0700

    changed redux server to be redux based - done due to electron writeup

--- comm/redux/Proxy.js
+++ comm/redux/Proxy.js
@@ -1,27 +1,27 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 
 // Proxy is only rendered in html DOM so window exists for sure
 
 export default class Proxy extends Component {
-    static propTypes = {
-        Component: React.Component,
-        id: PropTypes.string,
-        setSetState: PropTypes.func.isRequired,
-        dispatch: PropTypes.func.isRequired
-    }
+    // static propTypes = {
+    //     Component: React.Component,
+    //     id: PropTypes.string,
+    //     setSetState: PropTypes.func.isRequired,
+    //     dispatch: PropTypes.func.isRequired
+    // }
     mounted = false
     initialState = {}
     componentDidMount() {
         this.mounted = true;
-        let { setSetState } = this.props;
+        const { setSetState } = this.props;
         setSetState(this.setState.bind(this));
     }
     render() {
-        let { Component, dispatch } = this.props;
-        let state = this.state;
+        const { Component, dispatch } = this.props;
+        const state = this.state;
         if (!this.mounted) {
             // because on mount, state has not yet been received, so dont render
-            return <span />;
+            return null;
         } else {
             return <Component {...state} dispatch={dispatch} />
         }
--- comm/redux/index.js
+++ comm/redux/index.js
@@ -1,9 +1,9 @@
 // TODO: figure out how to make redux-offline only persist some keys, like there is no reason to persist messages
-
-import React, { Component, PropTypes } from 'react'
+import React from 'react'
 import { combineReducers, createStore } from 'redux'
 import { render, unmountComponentAtNode } from 'react-dom'
-import { Provider, connect } from 'react-redux'
+import { shallowEqual } from 'cmn/recompose' // eslint-disable-line no-unused-vars
+import { deepAccessUsingString } from 'cmn/all'
 // import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 // import { offline } from 'redux-offline'
 // import offlineConfigDefault from 'redux-offline/lib/defaults'
@@ -13,56 +13,18 @@ import { Provider, connect } from 'react-redux'
 import elements from './elements'
 import { removeElement, addElement } from './elements'
 
-import ElementServer from './ElementServer'
 import Proxy from './Proxy'
 
-// App is server that all proxies connect to
-const App = connect(
-    function(state) {
-        return {
-            elements: state.elements
-        }
-    }
-)(class AppClass extends Component {
-    // crossfile-link3138470
-    static propTypes = {
-        elements: PropTypes.array
-    }
-    render() {
-        let { elements } = this.props;
-        return (
-            <div>
-                { elements.map(element => <ElementServer key={element.id} {...element} />) }
-            </div>
-        )
-    }
-});
-
-// export function unmountProxiedElement(id, container) {
-//     // TODO:
-//     console.log('unmount element id:', id);
-
-//     if (container) unmountComponentAtNode(container);
-// }
-
-function renderProxiedElement(callInReduxPath, component, container, wanted) {
+function renderProxiedElement(callInReduxScope, server_name, component, container, wanted) {
     // this should imported and executed in the dom where we want to render the html element
     // if ReduxServer is in same scope, set callInReduxPath to gReduxServer
-    // resolves with elementid - so dever can use with unmountProxiedElement(id)
+    // resolves with elementid - so dever can use with dispatch(removeElement(id)) --- actually i changed it, it resolves with a helper function which internally does dispatch(removeElement(id)), see link7884721
     // component - react class
     // container - dom target - document.getElementById('root')
     // wanted - wanted state
-    // store.dispatch(addElement('todo', component.name, wanted));
+    // store.dispatch(addElement('todo', component.name, wanted));    if (Array.isArray(callInReduxPath)) {
 
-    let callInRedux;
-    if (Array.isArray(callInReduxPath)) {
-        let [callInReduxScope, server_name] = callInReduxPath;
-        callInRedux = (method, ...args) => callInReduxScope(server_name + '.' + method, ...args);
-    } else {
-        // no need for comm, we are in same scope
-        let server = callInReduxPath
-        callInRedux = (method, ...args) => server[method](...args);
-    }
+    const callInRedux = (method, ...args) => callInReduxScope(server_name + '.' + method, ...args);
 
     let resolveAfterMount; // resolves with unmount function
     const promise = new Promise(resolve => {
@@ -77,17 +39,16 @@ function renderProxiedElement(callInReduxPath, component, container, wanted) {
         callInRedux('dispatch', action);
     };
 
-    const unmountProxiedElement = function(nounmount, nodispatch) {
-        console.log('DOING unmountProxiedElement');
-        if (!nodispatch) dispatch(removeElement(id));
-        if (!nounmount) unmountComponentAtNode(container);
-    };
+    // const unmountProxiedElement = function(dontUnmount, dontDispatch) {
+    //     console.log('DOING unmountProxiedElement');
+    //     if (!dontDispatch) dispatch(removeElement(id, dontUnmount));
+    // };
 
     const progressor = function(aArg) {
-        let { __PROGRESS } = aArg;
+        const { __PROGRESS } = aArg;
 
         if (__PROGRESS) {
-            let { state } = aArg;
+            const { state } = aArg;
             if (id === undefined) {
                 id = aArg.id;
                 const setSetState = aSetState => {
@@ -95,62 +56,99 @@ function renderProxiedElement(callInReduxPath, component, container, wanted) {
                     setState(() => state);
                 };
                 render(<Proxy Component={component} id={id} setSetState={setSetState} dispatch={dispatch} />, container);
-                resolveAfterMount(unmountProxiedElement);
+                resolveAfterMount(dontUnmount => dispatch(removeElement(id, dontUnmount))); // link7884721
             } else {
                 setState(() => state);
             }
         } else {
             // unmounted - server was shutdown by unregister()
             console.log('ok unmounting in dom, aArg:', aArg);
-            unmountComponentAtNode(container);
+            if (!aArg || !aArg.dontUnmount) unmountComponentAtNode(container);
         }
     };
 
-    if (Array.isArray(callInReduxPath)) {
-        callInRedux('addElement', { wanted }, progressor);
-    } else {
-        // no need for comm, we are in same scope
-        callInRedux('addElement', { wanted }, fakeprog => { fakeprog.__PROGRESS = 1; progressor(fakeprog); }).then(progressor); // the .then is so it unmounts, as addElement returns promise, to keep Comm aReportProgress alive
-    }
+    callInRedux('addElement', { wanted }, progressor);
 
     // window is defintiely available, as renderProxiedElement is only used in DOM
-    window.addEventListener('unload', () => unmountProxiedElement(true), false);
+    window.addEventListener('unload', ()=>dispatch(removeElement(id, true)), false);
 
     return promise;
 }
 
-export class Server {
-    store = undefined
+class Server {
+    // store = undefined
+    // serverElement
     nextelementid = 0
-    constructor(reducers) {
+    removeElement = {} // holds promises, to trigger to remove element
+    constructor(reducers, serverElement) {
 
         // this.store = createStore(reducer, undefined, compose(applyMiddleware(thunk), offline(offlineConfigDefault)));
         // this.store = createStore(combineReducers(reducers), undefined, compose(applyMiddleware(thunk), offline(offlineConfigDefault)));
         // this.store = createStore(combineReducers(reducers), undefined, applyMiddleware(thunk));
         this.store = createStore(combineReducers({ ...reducers, elements }));
 
-        let container = document.createElement('div');
-        container.classList.add('redux-server');
-        document.body.appendChild(container);
-
-        render(<Provider store={this.store}><App/></Provider>, container);
+        this.store.subscribe(this.render);
+        this.serverElement = serverElement;
+        this.render();
     }
-    removeElement = {};
-    addElement(aArg, aReportProgress, ...args) {
-        console.log('in addElement, aArg:', aArg, 'aReportProgress:', aReportProgress, 'args:', args);
+    render = () => {
+        console.log('IN SERVER RENDER');
+        const state = this.store.getState();
+        const { elements } = state;
+
+        // check if any element was removed, if so then trigger this.removeElement which will resolve its hanging promise
+        for (const id of Object.keys(this.removeElement)) {
+            if (!(id in elements)) {
+                this.removeElement(id);
+            }
+        }
+
+        // TODO: shallowEqual here to figure out if i should update anything
+        for (const { /*id,*/ wanted, setState } of elements) {
+            setState(buildWantedState(wanted, state));
+        }
+        this.serverElement(state, this.store.dispatch); // equilavent of serverElement.setState(state)
+    }
+    addElement = (aArg, aReportProgress/*, ...args*/) => {
+        // console.log('in addElement, aArg:', aArg, 'aReportProgress:', aReportProgress, 'args:', args);
+        console.log('in addElement, aArg:', aArg);
         const id = (this.nextelementid++).toString(); // toString because it is used as a key in react - crossfile-link3138470
         return new Promise( resolve => { // i need to return promise, because if it is Comm, a promise will keep it alive so it keeps responding to aReportProgress
-            let { wanted } = aArg;
+            const { wanted } = aArg;
             const setState = state => aReportProgress({ id, state });
             this.store.dispatch(addElement(id, wanted, setState));
 
-            this.removeElement[id] = () => resolve({destroyed:true});
+            // this.removeElement[id] is only to be called by render as a result of dispatch(removeElement)
+            this.removeElement[id] = () => {
+                delete this.removeElement[id];
+                resolve({ destroyed:true });
+            };
         });
     }
     dispatch(aArg) {
-        let action = aArg;
+        const action = aArg;
         this.store.dispatch(action);
     }
 }
 
-export default renderProxiedElement;
+const DOTPATH_AS_PATT = /(.+) as (.+)$/m;
+function buildWantedState(wanted, state) {
+    console.log('state:', state, 'wanted:', wanted);
+    let wanted_state = {};
+    for (let dotpath of wanted) {
+        let name;
+        if (DOTPATH_AS_PATT.test(dotpath)) {
+            // ([, dotpath, name] = DOTPATH_AS_PATT.exec(dotpath));
+            let matches = DOTPATH_AS_PATT.exec(dotpath);
+            dotpath = matches[1];
+            name = matches[2];
+        } else {
+            name = dotpath.split('.');
+            name = name[name.length-1];
+        }
+        wanted_state[name] = deepAccessUsingString(state, dotpath, 'THROW');
+    }
+    return wanted_state;
+}
+
+export { Server, renderProxiedElement }
