9413ace33a362b478a2b7824296e440dae3580f3
Noitidart <noitidart@gmail.com>
2017-08-18 05:33:08 -0700

    needed index and export of each for jest

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as all from './all'
+import * as background from './background'
+import * as classnames from './classnames'
+import * as comm from './comm'
+import * as dom from './dom'
+import * as extensionPolyfill from './extension-polyfill'
+import * as fetchPolyfill from './fetch-polyfill'
+import * as immutable from './immutable'
+import * as qs from './qs'
+import * as recompose from './recompose'
+import * as window from './window'
+
+export { all, background, classnames, comm, dom, extensionPolyfill, fetchPolyfill, immutable, qs, recompose, window }
