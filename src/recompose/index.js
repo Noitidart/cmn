a344881972f7f665c6d7344d0ffc51a9d18441dd
Noitidart <noitidart@gmail.com>
2017-08-21 23:34:28 -0700

    in depth0Or1Equal if either is not an object, it returns shallowEqual

--- src/recompose/index.js
+++ src/recompose/index.js
@@ -101,6 +101,8 @@ function flattenDepth1(arr) {
 // depth0Or1Equal({a:[]}, {a:[]}) === false
 // depth0Or1Equal({a:[]}, {a:[]}, {a:1}) === true
 export function depth0Or1Equal(obj1, obj2, depth1Keys={}) {
+    if (!isObject(obj1) || !isObject(obj2)) return shallowEqual(obj1, obj2);
+
     // depth1Keys is objecect of keys you want to test at depth of 1, by default test depth of 0
     const keys1 = Object.keys(obj1);
     const keys2 = Object.keys(obj2);
