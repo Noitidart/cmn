b7c7a8c3720aa156d7c82416feab40774a38f9be
Noitidart <noitidart@gmail.com>
2017-08-22 12:09:22 -0700

    forgot to import StatusBar

--- src/react-native/index.js
+++ src/react-native/index.js
@@ -1,4 +1,4 @@
-import { Platform, Dimensions } from 'react-native'
+import { Platform, Dimensions, StatusBar } from 'react-native'
 
 export function isPortrait() {
     const { height, width } = Dimensions.get('screen');
@@ -19,8 +19,13 @@ export function getBarHeights() {
             const screen = Dimensions.get('screen');
             // const SCALE = window.scale; // same as get screen.scale
 
+            // console.log('window:', Dimensions.get('window'))
+            // console.log('screen:', Dimensions.get('screen'))
+            // console.log('StatusBarManager.HEIGHT:', NativeModules.StatusBarManager, 'StatusBar.currentHeight:', StatusBar.currentHeight)
             const SoftBarHeight = isPortrait() ? screen.height - window.height : screen.width - window.width;
             const StatusBarHeight = StatusBar.currentHeight;
+            // console.log('SoftBarHeight:', SoftBarHeight);
+            // console.log('StatusBarHeight:', StatusBarHeight, 'SoftBarHeight / SCALE:', SoftBarHeight / SCALE);
 
             return { status:StatusBarHeight, soft:SoftBarHeight };
         }
