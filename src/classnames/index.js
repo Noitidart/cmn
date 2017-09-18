dda92041a844254e520e75c80387c2741a1cfdaf
Noitidart <noitidart@gmail.com>
2017-09-17 20:00:59 -0700

    made classnames default export

--- src/classnames/index.js
+++ src/classnames/index.js
@@ -1,5 +1,5 @@
 import classnamesDefault from 'classnames'
-export function classnames(...args) {
+export default function classnames(...args) {
     // returns undefined instead of blank string for use with react per - https://github.com/JedWatson/classnames/issues/115#issuecomment-284296632
     return classnamesDefault(...args) || undefined;
 }
